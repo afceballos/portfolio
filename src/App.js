54e06b9717b77925e2c3a268bb178f524139716c
afceballos <andrescebvil@gmail.com>
2023-02-01 15:24:33 -0500

    Section Skills and Projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
@@ -9,6 +12,8 @@ function App() {
     <div className="App">
       <NavBar/>
       <Banner/>
+      <Skills/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,84 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCards";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+
+export const Projects = () => {
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & development",
+            imgUrl: projImg1,
+        },{
+            title: "Business Startup",
+            description: "Design & development",
+            imgUrl: projImg2,
+        },{
+            title: "Business Startup",
+            description: "Design & development",
+            imgUrl: projImg3,
+        },{
+            title: "Business Startup",
+            description: "Design & development",
+            imgUrl: projImg1,
+        },{
+            title: "Business Startup",
+            description: "Design & development",
+            imgUrl: projImg2,
+        },{
+            title: "Business Startup",
+            description: "Design & development",
+            imgUrl: projImg3,
+        }
+    ]
+
+    return (
+        <section className="project" id="project" >
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Projects</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first" >
+                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab One</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third" >
+                                    Disabled
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first" >
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return(
+                                                    <ProjectCard
+                                                        key={index}
+                                                        {...project}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second" >Lorem ipsum dolor sit amet</Tab.Pane>
+                                <Tab.Pane eventKey="third" >Lorem ipsum dolor sit amet</Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2} ></img>
+
+        </section>
+    );
+}
